4e468881795aa4b7e9d6b6beebe59306c6971b91
Kirill Mikhailov <kmikhailov@mail.ru>
2021-01-14 19:43:55 +0500

    order tests

--- src/ru/navilab/chessdevtest/ChessDevJUnitTest.java
+++ src/ru/navilab/chessdevtest/ChessDevJUnitTest.java
@@ -1,16 +1,17 @@
 package ru.navilab.chessdevtest;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import ru.navilab.chessdevtest.impl.ChessDevTestImpl;
 
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ChessDevJUnitTest {
 
     public static final String FIRST_CASE = "first case";
     public static final String SECOND_CASE = "second case";
 
     @Test
+    @Order(1)
     public void writeOnlyTest() {
         ChessDevTestImpl test = ChessDevTestImpl.createDefault();
         test.init();
@@ -19,20 +20,40 @@ public class ChessDevJUnitTest {
         test.close();
     }
 
-    private void testWriteString(ChessDevTestImpl test, String inputStream) {
-        int index = test.save(inputStream.getBytes());
-        byte[] bytes = test.get(index);
-        String outString = new String(bytes);
-        Assertions.assertEquals(inputStream, outString);
-    }
-
     @Test
+    @Order(2)
     public void readOnlyTest() {
         ChessDevTestImpl test = ChessDevTestImpl.createDefault();
         test.init();
         testReadString(test, FIRST_CASE, 1);
         testReadString(test, SECOND_CASE, 2);
         test.close();
+    }
+
+    @Test
+    @Order(3)
+    public void uppend3Test() {
+        ChessDevTestImpl test = ChessDevTestImpl.createDefault();
+        test.init();
+        testWriteString(test, SECOND_CASE + FIRST_CASE);
+        test.close();
+    }
+
+    @Test
+    @Order(4)
+    public void read3Test() {
+        ChessDevTestImpl test = ChessDevTestImpl.createDefault();
+        test.init();
+        testReadString(test, SECOND_CASE + FIRST_CASE, 3);
+        test.close();
+    }
+
+
+    private void testWriteString(ChessDevTestImpl test, String inputStream) {
+        int index = test.save(inputStream.getBytes());
+        byte[] bytes = test.get(index);
+        String outString = new String(bytes);
+        Assertions.assertEquals(inputStream, outString);
     }
 
     private void testReadString(ChessDevTestImpl test, String string, int index) {
